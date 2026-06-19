59f1893908b3700d84c35a42cdddca8216c870d4
Arya Suwandika <thethirdswan@gmail.com>
2026-06-19 21:04:34 +0800

    - rebalanced coal block processing in coker to make coke block - added coker recipes for coal, bit coal and lignite - rebalanced sugar mass create mixing recipe to use salt water - added low efficiency sugar crafting recipe from sugar mass - relocated wrought iron spindle recipes to hardrock namespace - made processing binding use `forge:glue` tag for it to accept TFC glue as well

--- server_scripts/recipes/remove.js
+++ server_scripts/recipes/remove.js
@@ -343,4 +343,5 @@ onEvent('recipes', event => {
 	// Go around wildcard removals
 	event.shapeless('immersiveengineering:wire_electrum', ['createaddition:electrum_wire']).id('kubejs:electrum_wire_conversion')
 	event.shapeless('9x minecraft:sugar', ['supplementaries:sugar_cube']).id('supplementaries:sugar_cube_uncrafting')
+	event.shapeless('minecraft:sugar', ['kubejs:sugar_mass']).id('kubejs:sugar')
 })
